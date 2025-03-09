2bb261989654200edec98a177fe0340aa97090f7
tadebiadi457 <tadebiadi457@gmail.com>
2025-03-09 19:50:54 +0000

    Add 4.php

---
+++ src/4.php
@@ -0,0 +1,12 @@
+ <?php
+// Your PHP Code Here
+
+$x = 10;
+if ($x < 5) {
+    echo "X is less than 5";
+} else if ($x == 5) {
+    echo "X is equal to 5";
+} else {
+    echo "X is greater than 5";
+}
+?>
