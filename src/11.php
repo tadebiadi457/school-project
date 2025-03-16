b3ffd9e4191b430b4de588d54c721c41fb7a4e0b
tadebiadi457 <tadebiadi457@gmail.com>
2025-03-16 05:10:09 +0000

    Add 11.php

---
+++ src/11.php
@@ -0,0 +1,31 @@
+<?php
+// Function to generate a random number between 1 and 10
+function get_random_number() {
+    return rand(1, 10);
+}
+
+// Function to check if a number is even or odd
+function is_even($num) {
+    return $num % 2 == 0;
+}
+
+// Function to calculate the sum of two numbers
+function add($a, $b) {
+    return $a + $b;
+}
+
+// Function to calculate the difference of two numbers
+function subtract($a, $b) {
+    return $a - $b;
+}
+
+// Function to calculate the product of two numbers
+function multiply($a, $b) {
+    return $a * $b;
+}
+
+// Function to calculate the quotient of two numbers
+function divide($a, $b) {
+    return $a / $b;
+}
+?>
